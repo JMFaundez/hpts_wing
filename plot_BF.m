75617604b7aeadd86f7f8069491f3a0d698cf35a
joselaptop <josfa@kth.se>
2023-10-31 15:11:55 +0100

    fixing name

--- plot_BF.m
+++ plot_BF.m
@@ -4,7 +4,7 @@ data = base_case('fringe_m50.f00001',230,30);
 
 XA = data.xx(1,:); % x coordinate points at the surface
 YA = data.yy(1,:); % y coordinate points at the surface
-[val0 inx0] = min(abs(XA));
+[val0 ix0] = min(abs(XA));
 dth = data.dth(ix0:end);
 xa = XA(ix0:end);
 
