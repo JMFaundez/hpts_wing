9ba31662d99aced460b6fcad42cc067b67008fcd
joselaptop <josfa@kth.se>
2023-10-31 15:13:40 +0100

    y99 plot

--- plot_BF.m
+++ plot_BF.m
@@ -6,10 +6,13 @@ XA = data.xx(1,:); % x coordinate points at the surface
 YA = data.yy(1,:); % y coordinate points at the surface
 [val0 ix0] = min(abs(XA));
 dth = data.dth(ix0:end);
+d99 = data.y99(ix0:end);
 xa = XA(ix0:end);
 
 figure()
+hold on
 plot(xa,dth)
+plot(xa,d99)
 xlabel('x')
 ylabel('\delta')
 
