d8eae48c89883a2463a6a59ea69afecb1ba95773
joselaptop <josfa@kth.se>
2023-10-31 17:41:49 +0100

    BOF

--- plot_BF.m
+++ plot_BF.m
@@ -16,11 +16,13 @@ bf_diego = load('baseFlow.mat');
 [mask,xcontour,ycontour] = blWeights(bf_diego);
 
 
+d_diego = sqrt((xcontour-bf_diego.X(:,1)).^2+(ycontour-bf_diego.Y(:,1)).^2) 
+
 figure()
 hold on
 plot(xa,3*real(dth),'DisplayName','3\delta^*')
 plot(xa,d99,'DisplayName','my d99')
-plot(xcontour-bf_diego.X(:,1),ycontour-bf_diego.Y(:,1),'DisplayName','diego')
+plot(xcontour,d_diego,'DisplayName','diego')
 legend()
 xlabel('x')
 ylabel('\delta')
